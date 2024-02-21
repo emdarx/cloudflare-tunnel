41c4e7af4c3a2b28e82158be3b4f661c813f6625
Amir Dark <amdark77@gmail.com>
2024-02-21 12:49:28 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '77c234bf-54a7-4cc2-9bef-462ee9b7ad0a';
+let userID = 'e979b8ad-13f4-4b5e-ba2a-d8d3bd112fee';
 let proxyIP = "49.51.46.56";
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
